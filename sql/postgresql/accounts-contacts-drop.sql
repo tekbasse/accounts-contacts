e4934ee662c090899df3e5cc66c46397fd66cc3b
Benjamin Brink <tekbasse@yahoo.com>
2018-08-07 09:35:25 -0700

    remove vendor/customer data model that belongs in accounts-ledger pkg

--- sql/postgresql/accounts-contacts-drop.sql
+++ sql/postgresql/accounts-contacts-drop.sql
@@ -1,21 +1,7 @@
 -- accounts-contacts-drop.sql
 --
 -- @ported from sql-ledger and combined with parts from OpenACS ecommerce package
--- @license GNU GENERAL PUBLIC LICENSE, Version 2, June 1991
-
 -- OpenACS core integration
-
-select acs_object__delete(object_id) from qal_vendor_object_id_map;
-select acs_object_type__drop_type('qal_grps_vendor','t');
-drop index qal_vendor_object_id_map_vendor_id_idx;
-drop index qal_vendor_object_id_map_instance_id_idx;
-DROP TABLE qal_vendor_object_id_map;
-
-select acs_object__delete(object_id) from qal_customer_object_id_map;
-select acs_object_type__drop_type('qal_grps_customer','t');
-drop index qal_customer_object_id_map_customer_id_idx;
-drop index qal_customer_object_id_map_instance_id_idx;
-DROP TABLE qal_customer_object_id_map;
 
 select acs_object__delete(object_id) from qal_contact_object_id_map;
 select acs_object_type__drop_type('qal_grps_contact','t');
@@ -23,41 +9,6 @@ drop index qal_contact_object_id_map_contact_id_idx;
 drop index qal_contact_object_id_map_instance_id_idx;
 DROP TABLE qal_contact_object_id_map;
 
-
--- App data model
-
-drop index qal_vendor_tax_trashed_p_idx;
-drop index qal_vendor_tax_vendor_id_idx;
-drop index qal_vendor_tax_instance_id_idx;
-
-DROP TABLE qal_vendor_tax;
-
-drop index qal_customer_tax_trashed_by_idx;
-drop index qal_customer_tax_customer_id_idx;
-drop index qal_customer_tax_instance_id_idx;
-
-DROP TABLE qal_customer_tax;
-
-drop index qal_sic_sic_type_idx;
-drop index qal_sic_code_idx;
-
-DROP TABLE qal_sic;
-
-drop index qal_vendor_trashed_p_idx;
-drop index qal_vendor_vendor_code_idx;
-drop index qal_vendor_id_idx;
-drop index qal_vendor_contact_id_idx;
-drop index qal_vendor_instance_id_idx;
-
-DROP TABLE qal_vendor;
-
-drop index qal_customer_trashed_p_idx;
-drop index qal_customer_customer_code_idx;
-drop index qal_customer_id_idx;
-drop index qal_customer_contact_id_idx;
-drop index qal_customer_instance_id_idx;
-
-DROP TABLE qal_customer;
 
 drop index qal_other_address_map_trashed_p_idx;
 drop index qal_other_address_map_address_id_idx;
