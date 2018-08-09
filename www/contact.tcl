9c9858f521ec219c1ac6cb2c015ad1c8c5915cc5
Benjamin Brink <tekbasse@yahoo.com>
2018-08-09 07:19:36 -0700

    add framework for UI.

---
+++ www/contact.tcl
@@ -0,0 +1,5 @@
+set title "Contact'"
+set context [list $title]
+
+# This is the page for displaying a single contact.
+# If no contact_id is supplied, page makes a new contact.
---
+++ www/contacts.tcl
@@ -0,0 +1,27 @@
+# accounts-contacts/www/tickets.tcl
+
+
+set title "#accounts-contacts.contact#"
+set context [list $title]
+
+
+set content_html ""
+
+set instance_id [qc_set_instance_id]
+set user_id [ad_conn user_id]
+# basic permission check to allow more precise permission error messages
+set read_p [permission::permission_p -party_id $user_id -object_id $instance_id -privilege read]
+
+#qc_permission_p user_id contact_id property_label privilege instance_id 
+#set read_p \[qc_permission_p $user_id $contact_id non_assets read $instance_id\]
+#set create_p \[qc_permission_p $user_id $contact_id non_assets create $instance_id\]
+#set write_p \[qc_permission_p $user_id $contact_id non_assets write $instance_id\]
+#set admin_p \[qc_permission_p $user_id $contact_id non_assets admin $instance_id\]
+#set delete_p \[qc_permission_p  $user_id $contact_id non_assets delete $instance_id\]
+
+set user_message_list [list ]
+
+
+set contact_ids_list [qc_contact_ids_for_user $user_id $instance_id]
+
+
