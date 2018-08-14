952998a48cc95699be7863a27f55345f036e8c35
Benjamin Brink <tekbasse@yahoo.com>
2018-08-14 08:43:55 -0700

    more code

--- www/contact.tcl
+++ www/contact.tcl
@@ -22,9 +22,22 @@ if { !$read_p } {
 set write_p [qc_permission_p $user_id "" $property_label write $instance_id]
 
 # defaults
-set input_array(qf_write_p) 0
-set input_array(qf_counter) 0
 set input_array(contact_id) ""
+#
+# If contact_id exists, show the contact if qf_write_p is 0
+# If qf_write_p is 1 and contact_id exists, then edit
+# If contact_id doesn't exist, show form if write_p is 1
+set input_array(qf_write_p) 0
+#
+# How to force qfo_g2 to present the form,
+# even if validated due to form_submitted_p?
+# One way is to have a form_submission counter.
+# This preserves the option to use hash_check with the form at some point.
+# On first form submit, validation is always 0, when write_p is used.
+# Which means we need to know if the variable write_p is referred to exists.
+set input_array(qf_counter) 0
+
+
 # Get input_array.
 set form_submitted_p [qf_get_inputs_as_array input_array]
 if { $input_array(qf_write_p) ne 0 } {
@@ -35,38 +48,69 @@ if { $input_array(qf_write_p) ne 0 } {
 }
 
 set contact_id $input_array(contact_id)
-if { $contact_id ne "" } {
-    set contact_list [qal_contact_read $contact_id]
-}
 
 if {[catch { set qf_counter [expr { $input_array(qf_counter) + 1 } ] } ] } {
     ns_log Warning "accounts-contacts/www/contact.tcl qf_counter + 1 error, qf_counter '${qf_counter}'. Reset to 0"
     set qf_counter 0
 }
-# If contact_id exists, show the contact if qf_write_p is 0
-# If qf_write_p is 1 and contact_id exists, then edit
-# If contact_id doesn't exist, show form if write_p is 1
 array unset input_array contact_id
 array unset input_array qf_write_p
 # Scope qf_write_p to permissions of write_p
 set qf_write_p [expr { $write_p && $qf_write_p } ]
-# Following logic won't work, because hash_check might be needed at some point
-#if { $qf_write_p eq 1 } {
-#    set form_submitted_p 0
-#}
 
-# How to force qfo_g2 to present the form,
-# even if validated due to form_submitted_p?
-# One way is to have a form_submission counter.
-# On first form submit, validation is always 0, when write_p is used..
-# Which means we need to know if the variable write_p is referred to exists.
 
 # Form field definitions
+set disabled_p [expr { !$qf_write_p } ]
+
+if { $qf_counter < 2 } {
+    if { $form_submitted_p eq 1 && $qf_counter eq 0 && $contact_id ne "" } {
+	set record_nvl [qal_contact_read $contact_id]
+    }
+    #  id - this is contact_id
+    #  rev_id
+    #  instance_id
+    #  parent_id
+    #  label
+    #  name
+    #  street_addrs_id
+    #  mailing_addrs_id
+    #  billing_addrs_id
+    #  vendor_id
+    #  customer_id
+    #  taxnumber
+    #  sic_code
+    #  iban
+    #  bic
+    #  language_code
+    #  currency
+    #  timezone
+    #  time_start
+    #  time_end
+    #  url
+    #  user_id
+    #  created
+    #  created_by
+    #  trashed_p
+    #  trashed_by
+    #  trashed_ts
+    #  notes 
+    foreach {n v} $record_nvl {
+	set $n $v
+    }
+    set form_submitted_p 0
+    
+}
+
+
+
+##code Should the uneditable values be included in form as hidden types?
+# yes. qf_ paradigm can hide data by default  add it to declarations in qfo_g2 that can be overwritten ofcourse.
+
 set f_lol [list \
 	       [list name qf_write_p form_tag_type input type hidden value 1 ] \
 	       [list name qf_counter form_tag_type input type hidden value $qf_counter ] \
-	       [list name label datatype text_nonempty maxlength 40 label "Label"] \
-	       [list name taxnumber datatype text maxlength 32 label "taxnumber"] \
+	       [list name label datatype text_nonempty maxlength 40 label "Label" value $label] \
+	       [list name taxnumber datatype text maxlength 32 label "taxnumber" value $taxnumber ] \
 	       [list name sic_code datatype text maxlength 15 label "SIC Code"] \
 	       [list name iban datatype text maxlength 34 label "IBAN"] \
 	       [list name bic datatype text maxlength 34 label "BIC"] \
@@ -77,12 +121,8 @@ set f_lol [list \
 	       [list name time_end datatype timestamp label "Time End"] \
 	       [list name url datatype url size 40 maxlength 200 label "URL"] \
 	       [list name notes datatype html_text cols 40 rows 5 label "Notes"] \
-	       [list type submit name submit value "#acs-kernel.common_Save#" datatype text_nonempty label "" disabled $qf_write_p] \
+	       [list type submit name submit value "#acs-kernel.common_Save#" datatype text_nonempty label "" disabled $disabled_p] \
 	      ]
-
-##code
-
-
 
 ::qfo::form_list_def_to_array \
     -list_of_lists_name f_lol \
@@ -90,9 +130,6 @@ set f_lol [list \
     -array_name f_arr \
     -ignore_parse_issues_p 0
 
-if { $qf_counter < 2 } {
-    set form_submitted_p 0
-}
 
 set validated_p [qfo_2g \
 		     -form_id 20180809 \
@@ -139,3 +176,6 @@ if { !$qf_write_p && $write_p } {
 ns_log Notice "accounts-contacts/www/contact.tcl array get input_array '[array get input_array]'"
 ns_log Notice "accounts-contacts/www/contact.tcl validated_p '${validated_p}'"
 ns_log Notice "accounts-contacts/www/contact.tcl f_lol '${f_lol}'"
+
+
+
