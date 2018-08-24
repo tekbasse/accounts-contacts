647f2ed2db011c50d1a5b4993c8dc366882a5deb
Benjamin Brink <tekbasse@yahoo.com>
2018-08-23 21:04:31 -0700

    more code, some localization keys

--- www/contacts.tcl
+++ www/contacts.tcl
@@ -45,12 +45,12 @@ ns_log Notice "accounts-contacts/www/contacts.tcl instance_id ${instance_id} con
 set table_lists [list ]
 foreach c_list $contacts_lists {
     lassign $c_list id rev_id instance_id parent_id label name street_addrs_id mailing_addrs_id billing_addrs_id vendor_id customer_id taxnumber sic_code iban bic language_code currency timezone time_start time_end url user_id created created_by trashed_p trashed_by trashed_ts notes
-    set t_list [ $label $name $taxnumber $sic_code $iban $bic $language_code $currency $timezone $time_start $time_end $url $notes ]
+    set t_list [list $label $name $taxnumber $sic_code $iban $bic $language_code $currency $timezone $time_start $time_end $url $notes ]
     lappend table_lists $t_list
 }
 
 
-set titles_list [list "#accounts_contacts.label#" "#accounts_contacts.name#" "#accounts_contacts.taxnumber#" "#accounts_contacts.sic_code#" "#accounts_contacts.iban#" "#accounts_contacts.bic#" "#accounts_contacts.language_code#" "#accounts_contacts.currency#" "#accounts_contacts.timezone#" "#accounts_contacts.time_start#" "#accounts_contacts.time_end#" "#accounts_contacts.url#" "#accounts_contacts.notes#" ]
+set titles_list [list "#accounts-contacts.label#" "#accounts-contacts.name#" "#accounts-contacts.taxnumber#" "#accounts-contacts.sic_code#" "#accounts-contacts.iban#" "#accounts-contacts.bic#" "#accounts-contacts.language_code#" "#accounts-contacts.currency#" "#accounts-contacts.timezone#" "#accounts-contacts.time_start#" "#accounts-contacts.time_end#" "#accounts-contacts.url#" "#accounts-contacts.notes#" ]
 
 set sort_type_list [list -ascii -ascii -ascii -ascii -ascii -ascii -ascii -ascii -ascii -ascii -ascii -ascii -ignore ]
 
