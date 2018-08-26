342f895141c74b1fb5bc28c2155a612c1c6419d1
Benjamin Brink <tekbasse@yahoo.com>
2018-08-26 10:20:06 -0700

    add more code

--- www/contact.tcl
+++ www/contact.tcl
@@ -18,9 +18,10 @@ ns_log Notice "contact.tcl.11 instance_id '${instance_id}'"
 # in accounts-contacts, differentiate org_contact_id from contact_id
 set org_contact_id [qc_set_contact_id ]
 ns_log Notice "contact.tcl.14 instance_id '${instance_id}'"
-# contact_id is upvar'd by qc_set_instance_id. Here, we don't want it.
-unset contact_id
-##code in future, mac qc_set_instance_id upvar a declared name..??
+# contact_id is upvar'd by qc_set_contact_id. Here, it creates name conflict.
+#unset contact_id, set it to "" for logging.
+set contact_id ""
+
 
 
 set input_array(contact_id) ""
@@ -31,7 +32,7 @@ set input_array(contact_id) ""
 ns_log Notice "contact.tcl.30 instance_id '${instance_id}'"
 set property_label [qc_parameter_get propertyLabel $instance_id "org_accounts"]
 
-set write_p 0
+
 set read_p [qc_permission_p $user_id $org_contact_id $property_label read $instance_id]
 set content_html ""
 
@@ -48,6 +49,11 @@ set write_p [qc_permission_p $user_id $org_contact_id $property_label write $ins
 # If qf_write_p is 1 and contact_id exists, then edit
 # If contact_id doesn't exist, show form if write_p is 1
 set input_array(qf_write_p) 0
+# If qf_trash_p is not 0 , trash contact_id
+set input_array(qf_trash_p) 0
+# If qf_archive_p is not 0, archive contact_id
+set input_array(qf_archive_p) 0
+
 #
 # How to force qfo_g2 to present the form,
 # even if validated due to form_submitted_p?
@@ -62,10 +68,20 @@ set input_array(qf_counter) 0
 set form_submitted_p [qf_get_inputs_as_array input_array]
 if { $input_array(qf_write_p) ne 0 } {
     # Compensate for internationalization of value, convert to 1
+    # Edit mode
     set qf_write_p 1
 } else {
     set qf_write_p 0
+    # maybe trash or archive, but not both
+    if { $input_array(qf_archive_p) ne 0 } {
+	set input_array(qf_archive_p) 1
+
+    } elseif { $input_array(qf_trash_p) ne 0 } {
+	set input_array(qf_trash_p) 1
+    }	      
 }
+set qf_archive_p $input_array(qf_archive_p)
+set qf_trash_p $input_array(qf_trash_p)
 
 if { [qf_is_natural_number $input_array(id) ] } {
     set contact_id $input_array(contact_id)
@@ -77,6 +93,8 @@ if {[catch { set qf_counter [expr { $input_array(qf_counter) + 1 } ] } ] } {
     ns_log Warning "accounts-contacts/www/contact.tcl qf_counter + 1 error, qf_counter '${qf_counter}'. Reset to 0"
     set qf_counter 0
 }
+array unset input_array qf_archive_p
+array unset input_array qf_trash_p
 array unset input_array contact_id
 array unset input_array qf_write_p
 # Scope qf_write_p to permissions of write_p
@@ -90,44 +108,75 @@ ns_log Notice "contact.tcl.71 contact_id '${contact_id}' "
 
 ns_log Notice "contact.tcl.73 qf_write_p '${qf_write_p}' disabled_p '${disabled_p}' qf_counter '${qf_counter}' form_submitted_p '${form_submitted_p}' instance_id '${instance_id}'"
 if { $qf_counter < 2 } {
-    if { $form_submitted_p eq 1 && $qf_counter < 2 && $contact_id ne "" } {
-
-	set record_nvl [qal_contact_read $contact_id $org_contact_id]
-	ns_log Notice "contact.tcl.87 record_nvl '${record_nvl}'"
-	#  id - this is contact_id
-	#  rev_id *internal
-	#  instance_id
-	#  parent_id
-	#  label
-	#  name
-	#  street_addrs_id
-	#  mailing_addrs_id
-	#  billing_addrs_id
-	#  vendor_id
-	#  customer_id
-	#  taxnumber
-	#  sic_code
-	#  iban
-	#  bic
-	#  language_code
-	#  currency
-	#  timezone
-	#  time_start
-	#  time_end
-	#  url
-	#  user_id *
-	#  created *
-	#  created_by *
-	#  trashed_p *
-	#  trashed_by *
-	#  trashed_ts *
-	#  notes 
-	foreach {n v} $record_nvl {
-	    set $n $v
-	}
-    }
     set form_submitted_p 0
-    
+    if { $form_submitted_p eq 1 && $qf_counter < 2 && $contact_id ne "" } {
+	
+	if { $qf_trash_p } {
+	    set success_p [qal_contact_trash $contact_id]
+	} else { 
+	    set record_nvl [qal_contact_read $contact_id $org_contact_id]
+	    ns_log Notice "contact.tcl.87 record_nvl '${record_nvl}'"
+	    #  id - this is contact_id
+	    #  rev_id *internal
+	    #  instance_id
+	    #  parent_id
+	    #  label
+	    #  name
+	    #  street_addrs_id
+	    #  mailing_addrs_id
+	    #  billing_addrs_id
+	    #  vendor_id
+	    #  customer_id
+	    #  taxnumber
+	    #  sic_code
+	    #  iban
+	    #  bic
+	    #  language_code
+	    #  currency
+	    #  timezone
+	    #  time_start
+	    #  time_end
+	    #  url
+	    #  user_id *
+	    #  created *
+	    #  created_by *
+	    #  trashed_p *
+	    #  trashed_by *
+	    #  trashed_ts *
+	    #  notes 
+	    foreach {n v} $record_nvl {
+		set $n $v
+	    }
+	}
+	if { $qf_archive_p } {
+	    # update / close record
+	    # time is in standard date format.
+	    set input_array(time_end) [qf_clock_format [clock seconds]]
+	    set r_contact_id [qal_contact_write input_array]
+	    if { $contact_id eq "" } {
+		append content_html "<p>#acs-tcl.lt_We_had_a_problem_proc# #acs-tcl.Succes_error_submitted#</p>"
+		ns_log Warning "accounts-contacts/www/contact.tcl.157 unable to archive contact_id '${contact_id}"
+		set form_submitted_p 1
+	    } else {
+		ad_returnredirect contacts
+		ad_script_abort
+	    }
+	    
+	} elseif { $qf_trash_p } {
+	    set r_contact_id [qal_contact_trash $contact_id ]
+	    if { $contact_id eq "" } {
+		set message "<p>#acs-tcl.lt_We_had_a_problem_proc# #acs-tcl.Succes_error_submitted#</p>"
+		ns_log Warning "accounts-contacts/www/contact.tcl.157 unable to archive contact_id '${contact_id}"
+		ad_returnredirect -message $message contacts
+	    } else {
+		ad_returnredirect contacts
+	    }
+	    ad_script_abort
+	}
+    } elseif { $form_submitted_p eq 0 && $contact_id eq "" && $write_p } {
+	# Force to show a new record form
+	set qf_write_p 1
+    }
 }
 
 
@@ -178,34 +227,55 @@ set validated_p [qfo_2g \
 
 
 if { !$qf_write_p && $write_p } {
-    # Show button to edit contact record
-    append content_html [qf_button_form \
-			     name qf_write_p \
-			     value "#accounts-contacts.Edit_contact#" \
-			     id contact-20180810c \
-			     id contact-20180810c \
-			     action contact \
-			     name contact_id \
-			     value $contact_id ]
-    
-    
-    # Show buttons to manage addresses
-    #
-    append content_html [qf_button_form \
-			     name submit \
-			     value  "#accounts-contacts.Manage_street_addresses#" \
-			     id contact-20180810a \
-			     action contact-addresses \
-			     name contact_id \
-			     value $contact_id ]
-
-    append content_html [qf_button_form \
-			     id contact-20180810b \
-			     action contact-other-addresses \
-			     name submit \
-			     value "#accounts-contacts.Manage_other_addresses#" \
-			     name contact_id \
-			     value $contact_id ]
+    if { $contact_id ne "" } {
+	# Show button to edit contact record
+	append content_html [qf_button_form \
+				 name qf_write_p \
+				 value "#accounts-contacts.Edit_contact#" \
+				 id contact-20180810c \
+				 id contact-20180810c \
+				 action contact \
+				 name contact_id \
+				 value $contact_id ]
+	
+	# Show button to archive contact record
+	append content_html [qf_button_form \
+				 name qf_archive_p \
+				 value "#accounts-contacts.Archive_contact#" \
+				 id contact-20180826a \
+				 id contact-20180826a \
+				 action contact \
+				 name contact_id \
+				 value $contact_id ]
+	
+	# Show button to trash contact record
+	append content_html [qf_button_form \
+				 name qf_trash_p \
+				 value "#accounts-contacts.Trash_contact#" \
+				 id contact-20180826b \
+				 id contact-20180826b \
+				 action contact \
+				 name contact_id \
+				 value $contact_id ]
+	
+	# Show buttons to manage addresses
+	#
+	append content_html [qf_button_form \
+				 name submit \
+				 value  "#accounts-contacts.Manage_street_addresses#" \
+				 id contact-20180810a \
+				 action contact-addresses \
+				 name contact_id \
+				 value $contact_id ]
+	
+	append content_html [qf_button_form \
+				 id contact-20180810b \
+				 action contact-other-addresses \
+				 name submit \
+				 value "#accounts-contacts.Manage_other_addresses#" \
+				 name contact_id \
+				 value $contact_id ]
+    }
 }
 
 ns_log Notice "accounts-contacts/www/contact.tcl array get input_array '[array get input_array]'"
