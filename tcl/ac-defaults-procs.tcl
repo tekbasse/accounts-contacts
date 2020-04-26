a0eed3132990da40eb37cd7d7faf2c3fa75f29fc
Benjamin Brink <tekbasse@yahoo.com>
2020-04-26 13:31:54 -0700

    revise

--- tcl/ac-defaults-procs.tcl
+++ tcl/ac-defaults-procs.tcl
@@ -85,17 +85,19 @@ ad_proc -private qal_contact_form_def {
     set html_before3 {<div class="grid-6 m-grid-6 s-grid-6"><div class="content-box">}
     set html_after {</div></div>}
     
-    qal_contact_defaults fv_arr
-    
+    qal_contact_defaults fc_arr
+    qal_address_defaults fa_arr
     set f_lol [list \
-                   [list name label value $fv_arr(label) context content_c1 datatype text_word html_before $html_before1 html_after $html_after label "#accounts-contacts.label#" title "#q-data-types.alphanum_word_hint#"] \
-                   [list name name value $fv_arr(name) context content_c1 datatype text_nonempty html_before $html_before1 html_after $html_after label "#accounts-contacts.name#"] ]
+                   [list name label value $fc_arr(label) context content_c1 datatype text_word html_before $html_before1 html_after $html_after label "#accounts-contacts.label#" title "#q-data-types.alphanum_word_hint#"] \
+                   [list name name value $fc_arr(name) context content_c1 datatype text_nonempty html_before $html_before1 html_after $html_after label "#accounts-contacts.name#"] ]
     
     #
     # Make a subset of fields into a row, expandable to multiple rows.
     #
     
     # Make a list of choices for address_type
+    # Could almost use qal_address_type_keys here,
+    # but language keys don't correlate
     set at_lol [list \
                     [list value street_address label "#accounts-contacts.Address#"] \
                     [list value billing_address label "#accounts-contacts.Billing_Address"] \
@@ -104,7 +106,7 @@ ad_proc -private qal_contact_form_def {
     # set the value to existing one 
     set addrs_type_lol [list ]
     foreach at_list $at_lol {
-        if { $fv_arr(address_type) eq [lindex $at_list 1] } {
+        if { $fa_arr(address_type) eq [lindex $at_list 1] } {
             lappend at_list selected 1
         }
         lappend addrs_type_lol $at_list
@@ -112,33 +114,33 @@ ad_proc -private qal_contact_form_def {
 
     
     set addrs_lol  [list \
-                        [list name address_type value $fv_arr(address_type) context content_c2 datatype qf_choice $addrs_type_lol html_before $html_before1 html_after $html_after ] \
-                        [list name address0 value $fv_arr(address0) context content_c2 datatype text maxlength 32 html_before $html_before1 html_after $html_after label "#accounts-contacts.street_address# 1/3" title "#q-data-types.street_address_hint#"] \
-                        [list name address1 value $fv_arr(address1) context content_c2 datatype text maxlength 32 html_before $html_before1 html_after $html_after label "#accounts-contacts.street_address# 2/3" title "#q-data-types.street_address_hint#"] \
-                        [list name address2 value $fv_arr(address2) context content_c2 datatype text maxlength 32 html_before $html_before1 html_after $html_after label "#accounts-contacts.street_address# 3/3" title "#q-data-types.street_address_hint#"] \
-                        [list name city value $fv_arr(city) context content_c2 datatype text maxlength 32 html_before $html_before2 html_after $html_after label "#accounts-contacts.City#"] \
-                        [list name state value $fv_arr(state) context content_c2 datatype region maxlength 32 html_before $html_before2 html_after $html_after label "#q-data-types.region#"] \
-                        [list name postal_code value $fv_arr(postal_code) context content_c2 datatype postal_code maxlength 10 html_before $html_before2 html_after $html_after label "#q-data-types.postal_code#"] \
-                        [list name country_code value $fv_arr(country_code) context content_c2 datatype country_code maxlength 32 html_before $html_before3 html_after $html_after label "#q-data-types.country_code#"] \
-                        [list name attn value $fv_arr(attn) context content_c2 datatype text maxlength 32 html_before $html_before3 html_after $html_after label "#q-data-types.attn#" title "#q-data-types.attn_hint#"] \
-                        [list name phone  value $fv_arr(phone) context content_c2 datatype phone_number html_before $html_before2 html_after $html_after label "#q-data-types.phone_number#"] \
-                        [list name phone_time value $fv_arr(phone_time) context content_c2 datatype text html_before $html_before2 html_after $html_after label "#accounts-contacts.phone_time#" title "#accounts-contacts.phone_time_hint#" ] \
-                        [list name fax value $fv_arr(fax) context content_c2 datatype phone_number html_before $html_before2 html_after $html_after label "#accounts-contacts.Fax#"] \
-                        [list name email value $fv_arr(email) context content_c2 datatype email html_before $html_before2 html_after $html_after label "#accounts-contacts.Email#"] \
-                        [list name cc value $fv_arr(cc) context content_c2 datatype email html_before $html_before2 html_after $html_after label "#accounts-contacts.Cc#"] \
-                        [list name bcc value $fv_arr(bcc) context content_c2 datatype email html_before $html_before2 html_after $html_after label "#accounts-contacts.Bcc#"] ]
+                        [list name address_type value $fa_arr(address_type) context content_c2 datatype qf_choice $addrs_type_lol html_before $html_before1 html_after $html_after ] \
+                        [list name address0 value $fa_arr(address0) context content_c2 datatype text maxlength 32 html_before $html_before1 html_after $html_after label "#accounts-contacts.street_address# 1/3" title "#q-data-types.street_address_hint#"] \
+                        [list name address1 value $fa_arr(address1) context content_c2 datatype text maxlength 32 html_before $html_before1 html_after $html_after label "#accounts-contacts.street_address# 2/3" title "#q-data-types.street_address_hint#"] \
+                        [list name address2 value $fa_arr(address2) context content_c2 datatype text maxlength 32 html_before $html_before1 html_after $html_after label "#accounts-contacts.street_address# 3/3" title "#q-data-types.street_address_hint#"] \
+                        [list name city value $fa_arr(city) context content_c2 datatype text maxlength 32 html_before $html_before2 html_after $html_after label "#accounts-contacts.City#"] \
+                        [list name state value $fa_arr(state) context content_c2 datatype region maxlength 32 html_before $html_before2 html_after $html_after label "#q-data-types.region#"] \
+                        [list name postal_code value $fa_arr(postal_code) context content_c2 datatype postal_code maxlength 10 html_before $html_before2 html_after $html_after label "#q-data-types.postal_code#"] \
+                        [list name country_code value $fa_arr(country_code) context content_c2 datatype country_code maxlength 32 html_before $html_before3 html_after $html_after label "#q-data-types.country_code#"] \
+                        [list name attn value $fa_arr(attn) context content_c2 datatype text maxlength 32 html_before $html_before3 html_after $html_after label "#q-data-types.attn#" title "#q-data-types.attn_hint#"] \
+                        [list name phone  value $fa_arr(phone) context content_c2 datatype phone_number html_before $html_before2 html_after $html_after label "#q-data-types.phone_number#"] \
+                        [list name phone_time value $fa_arr(phone_time) context content_c2 datatype text html_before $html_before2 html_after $html_after label "#accounts-contacts.phone_time#" title "#accounts-contacts.phone_time_hint#" ] \
+                        [list name fax value $fa_arr(fax) context content_c2 datatype phone_number html_before $html_before2 html_after $html_after label "#accounts-contacts.Fax#"] \
+                        [list name email value $fa_arr(email) context content_c2 datatype email html_before $html_before2 html_after $html_after label "#accounts-contacts.Email#"] \
+                        [list name cc value $fa_arr(cc) context content_c2 datatype email html_before $html_before2 html_after $html_after label "#accounts-contacts.Cc#"] \
+                        [list name bcc value $fa_arr(bcc) context content_c2 datatype email html_before $html_before2 html_after $html_after label "#accounts-contacts.Bcc#"] ]
     
     set f2_lol [qfo::form_list_def_to_css_table_rows -list_of_lists_name f_lol -form_field_defs_to_multiply addrs_lol -rows_count 1]
     
     # Add the rest of the form elements. Skipping: taxnumber, iban, bic, currency
     set f3_lol [list \
-                    [list name sic_code value $fv_arr(sic_code) context content_c3 datatype natural_num html_before $html_before2 html_after $html_after label "#accounts-contacts.SIC#"] \
-                    [list name language_code value $fv_arr(language_code) context content_c3 datatype text maxlength 12 html_before $html_before2 html_after $html_after label "#accounts-contacts.language_code#" ] \
-                    [list name timezone value $fv_arr(timezone) context content_c3 datatype text maxlength 4 html_before $html_before2 html_after $html_after label "#accounts-contacts.timezone#" ] \
-                    [list name time_start value $fv_arr(time_start) context content_c3 datatype date html_before $html_before3 html_after $html_after label "#accounts-contacts.Startdate#" ] \
-                    [list name time_end value $fv_arr(time_end) context content_c3 datatype date html_before $html_before3 html_after $html_after label "#accounts-contacts.Enddate#" ] \
-                    [list name url value $fv_arr(url) context content_c3 datatype url html_before $html_before3 html_after $html_after label "#accounts-contacts.URL#" ] \
-                    [list name notes value $fv_arr(notes) context content_c4 datatype block_text cols 34 html_before $html_before3 html_after $html_after label "#accounts-contacts.notes#"] \
+                    [list name sic_code value $fc_arr(sic_code) context content_c3 datatype natural_num html_before $html_before2 html_after $html_after label "#accounts-contacts.SIC#"] \
+                    [list name language_code value $fc_arr(language_code) context content_c3 datatype text maxlength 12 html_before $html_before2 html_after $html_after label "#accounts-contacts.language_code#" ] \
+                    [list name timezone value $fc_arr(timezone) context content_c3 datatype text maxlength 4 html_before $html_before2 html_after $html_after label "#accounts-contacts.timezone#" ] \
+                    [list name time_start value $fc_arr(time_start) context content_c3 datatype date html_before $html_before3 html_after $html_after label "#accounts-contacts.Startdate#" ] \
+                    [list name time_end value $fc_arr(time_end) context content_c3 datatype date html_before $html_before3 html_after $html_after label "#accounts-contacts.Enddate#" ] \
+                    [list name url value $fc_arr(url) context content_c3 datatype url html_before $html_before3 html_after $html_after label "#accounts-contacts.URL#" ] \
+                    [list name notes value $fc_arr(notes) context content_c4 datatype block_text cols 34 html_before $html_before3 html_after $html_after label "#accounts-contacts.notes#"] \
                     [list type submit name save context content_c5 \
                          value "\#acs-kernel.common_save\#" datatype text html_before $html_before3 html_after $html_after label "" class "btn-big"] \
                     [list type submit name update context content_c5 \
