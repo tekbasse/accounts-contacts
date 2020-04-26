17f204c48351da1b9582a29ae1821924b6518574
Benjamin Brink <tekbasse@yahoo.com>
2020-04-26 13:29:18 -0700

    revise for documentation and user/admin demo

--- www/admin/test-qfo_g2.tcl
+++ www/admin/test-qfo_g2.tcl
@@ -11,18 +11,28 @@ if { !$admin_p } {
     ad_script_abort
 }
 
-set one_choice_tag_attribute_list [list [list label " label1 " value debit1] [list label " label2 " value debit2] [list label " label3 " value debit3] ]
+set one_choice_tag_attribute_list [list [list label " debitcard 1 " value debit1] [list label " debitcard 2 " value debit2] [list label " debitcard 3 " value debit3] ]
 
-set multi_choice_tag_attribute_list [list [list name card1 label " label 1 " value plastic1 selected 1] [list name card2 label " label 2 " value plastic2 selected 0] [list name card3 label " label 3 " value plastic3] ]
+set multi_choice_tag_attribute_list [list \
+                                         [list name card1 label " Bank 1 " value plastic1 selected 1] \
+                                         [list name card2 label " Bank 2 " value plastic2 selected 0] \
+                                         [list name card3 label " Bank 3 " value plastic3] ]
 
+
+# to switch multiple between checkbox and select, switch value checkbox and select.
+# to make them work identical,
+# change all the names in multi_choice_tag_attribute_list to be the same
+# as value as the select name's value.
 set f_lol [list \
                [list type text value "example value" name "input_text" label "input text" size 40 maxlength 80 ] \
-               [list type select name creditcard value $one_choice_tag_attribute_list ] \
-               [list type select value $multi_choice_tag_attribute_list multiple 1] \
+               [list type select name creditcard value ${one_choice_tag_attribute_list}] \
+               [list type checkbox name test value ${multi_choice_tag_attribute_list} multiple 1] \
                [list type submit name charlie value bravo tabindex 9 datatype text_nonempty] \
                [list tabindex 8 type submit name submit value "#acs-kernel.common_Save#" datatype text_nonempty]
-              ]
-
+          ]
+#ns_log Notice "[ad_conn url] one_choice_tag_attr.. '$one_choice_tag_attribute_list'"
+#ns_log Notice "[ad_conn url] multi_choice_tag_attr.. '$multi_choice_tag_attribute_list'"
+#ns_log Notice "[ad_conn url] f_lol '$f_lol'"
 set form_html ""
 qfo::form_list_def_to_array \
     -array_name f_arr \
@@ -34,10 +44,10 @@ set validated_p [qfo_2g \
                      -fields_array f_arr \
                      -form_varname form_html \
                      -hash_check 1]
-append content "<pre>\n"
+
 append content $form_html
-append content " &nbsp; &nbsp; &nbsp; <a href=\"test-qfo_g2\">clear</a>"
-append content "</pre>"
+append content "<span>  &nbsp; &nbsp; &nbsp; </span> <a href=\"test-qfo_g2\">clear</a>"
+
 
 if { $validated_p } {
     # output inputs
@@ -46,7 +56,7 @@ if { $validated_p } {
     foreach name [array names qfi_arr] {
         append content "<li>'${name}' : '$qfi_arr(${name})'</li>\n"
     }
-    append content "</ul>"
+    append content "</ul></pre>"
 
 
 } else {
