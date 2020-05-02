622100180bb074b106c6226dae405fa966b85b26
Benjamin Brink <tekbasse@yahoo.com>
2020-05-02 08:17:29 -0700

    add a view of contact for sharing w/o UI

---
+++ www/contact-view.tcl
@@ -0,0 +1,8 @@
+# <!-- this is a page for printing /viewing contact record without UI -->
+# todo
+
+
+# Make a contact-view page to just see (with no edit button)
+# for printouts and the like. Have the logo or something an anchor
+# to click back out.. without any highlighted boarder. Maybe whole page?
+# or just close the window for now
